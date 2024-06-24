7ca0200e8b5b478167327a4f96646f37ddc06219
Mittapalli Praneeth <sunnypraneeth3119@gmail.com>
2024-06-24 20:42:28 +0530

    first commit

---
+++ app/src/main/java/com/example/farmerpriceapp/FarmerCommunity.java
@@ -0,0 +1,24 @@
+package com.example.farmerpriceapp;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FarmerCommunity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_community);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+    }
+}
